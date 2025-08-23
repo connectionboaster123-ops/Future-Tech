ea004c7adc7f860ce784a62c11568862d5a0d0b9
connectionboaster123-ops <connectionboaster123@gmail.com>
2025-08-23 19:58:05 +0300

    Update tech.py

--- tech.py
+++ tech.py
@@ -1,16 +1,18 @@
-import phonenumbers                                                                                   from phonenumbers import geocoder, carrier, timezone
+import phonenumbers
+from phonenumbers import geocoder, carrier, timezone
 
 print("Phone number Validation")
 
 def track():
     try:
         user = input("Enter your name to use the tool: ")
-        number = input(f"Hello {user}, Enter number to validate (e.g., +2567000000): ")
+        number = input(f"Hello {user}, Enter number to validate (e.g., +256700000000): ")
 
         parsed_number = phonenumbers.parse(number, None)
         status = phonenumbers.is_valid_number(parsed_number)
-        region = geocoder.description_for_number(parsed_number, "en")                                         time_zones = timezone.time_zones_for_number(parsed_number)
+        region = geocoder.description_for_number(parsed_number, "en")
         phone_carrier = carrier.name_for_number(parsed_number, "en")
+        time_zones = timezone.time_zones_for_number(parsed_number)
 
         print(f"\nHello {user}, this is the result of the phone number:")
         print(f"Valid: {status}")
@@ -22,13 +24,15 @@ def track():
         print("Error occurred:", e)
     else:
         print(f"Successfully validated the phone number {number}")
+
 def main():
-      print("Welcome on board")
-      choice = str(input("Do u want to track a number (yes /no )..."))
-      if choice in ["Yes","YES","yes"]:
-          track()
-      elif choice in ["No","no","NO"]:
-          print("program stopped")
-      else:
-          print("Invalid choice")
+    print("Welcome on board")
+    choice = input("Do you want to track a number (yes/no): ").strip().lower()
+    if choice == "yes":
+        track()
+    elif choice == "no":
+        print("Program stopped")
+    else:
+        print("Invalid choice")
+
 main()
