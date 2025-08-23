98a4fa45d6c0effa5c3d39f8bbccfc7c3c1cb71f
connectionboaster123-ops <connectionboaster123@gmail.com>
2025-08-23 19:43:28 +0300

    Update tech.py

--- tech.py
+++ tech.py
@@ -1,13 +1,34 @@
-from flask import Flask, render_template, redirect, url_for
-app =Flask(__name__)
-@app.route('/')
-def home():
-   return render_template("home.html")
-@app.route('/about')
-def about():
-   return render_template('about.html')
-@app.route('/home')
+import phonenumbers                                                                                   from phonenumbers import geocoder, carrier, timezone
+
+print("Phone number Validation")
+
+def track():
+    try:
+        user = input("Enter your name to use the tool: ")
+        number = input(f"Hello {user}, Enter number to validate (e.g., +2567000000): ")
+
+        parsed_number = phonenumbers.parse(number, None)
+        status = phonenumbers.is_valid_number(parsed_number)
+        region = geocoder.description_for_number(parsed_number, "en")                                         time_zones = timezone.time_zones_for_number(parsed_number)
+        phone_carrier = carrier.name_for_number(parsed_number, "en")
+
+        print(f"\nHello {user}, this is the result of the phone number:")
+        print(f"Valid: {status}")
+        print(f"Region: {region}")
+        print(f"Time Zones: {time_zones}")
+        print(f"Carrier: {phone_carrier}")
+
+    except Exception as e:
+        print("Error occurred:", e)
+    else:
+        print(f"Successfully validated the phone number {number}")
 def main():
-   return redirect(url_for('home'))
-if __name__ == "__main__":
-   app.run(debug=True)
+      print("Welcome on board")
+      choice = str(input("Do u want to track a number (yes /no )..."))
+      if choice in ["Yes","YES","yes"]:
+          track()
+      elif choice in ["No","no","NO"]:
+          print("program stopped")
+      else:
+          print("Invalid choice")
+main()
